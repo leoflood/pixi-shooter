252d01d3da5acbb36eaeec66338a0b2544300836
unknown <leoflood98@gmail.com>
2022-09-18 20:25:12 +0100

    Files route changed

--- src/game/sounds.ts
+++ src/game/sounds.ts
@@ -1,4 +1,4 @@
-import { Howl } from 'howler';
+import { Howl } from "howler";
 
 interface ISounds {
   [key: string]: Howl;
@@ -6,24 +6,24 @@ interface ISounds {
 
 const sounds: ISounds = {
   // Music
-  'music-intro': new Howl({
-    src: ['/snd/music-intro.mp3'],
+  "music-intro": new Howl({
+    src: ["/pixi-shooter/snd/music-intro.mp3"],
     preload: true,
     loop: true,
   }),
-  'music-intro-2': new Howl({
-    src: ['/snd/music-intro-2.mp3'],
+  "music-intro-2": new Howl({
+    src: ["/pixi-shooter/snd/music-intro-2.mp3"],
     preload: true,
     loop: true,
   }),
-  'music-epic': new Howl({
-    src: ['/snd/music-epic.mp3'],
+  "music-epic": new Howl({
+    src: ["/pixi-shooter/snd/music-epic.mp3"],
     preload: true,
     volume: 1 / 3,
     loop: true,
   }),
-  'music-dark': new Howl({
-    src: ['/snd/music-dark.mp3'],
+  "music-dark": new Howl({
+    src: ["/pixi-shooter/snd/music-dark.mp3"],
     preload: true,
     volume: 1 / 3,
     loop: true,
@@ -31,47 +31,47 @@ const sounds: ISounds = {
 
   // Effects
   ak47: new Howl({
-    src: ['/snd/ak47.wav'],
+    src: ["/pixi-shooter/snd/ak47.wav"],
     preload: true,
     volume: 0.2,
   }),
   shotgun: new Howl({
-    src: ['/snd/shotgun.wav'],
+    src: ["/pixi-shooter/snd/shotgun.wav"],
     preload: true,
     volume: 0.2,
   }),
   pain: new Howl({
-    src: ['/snd/pain.wav'],
+    src: ["/pixi-shooter/snd/pain.wav"],
     preload: true,
     volume: 0.2,
   }),
   heal: new Howl({
-    src: ['/snd/heal.wav'],
+    src: ["/pixi-shooter/snd/heal.wav"],
     preload: true,
     volume: 0.2,
   }),
-  'wall-hit': new Howl({
-    src: ['/snd/wall-hit.wav'],
+  "wall-hit": new Howl({
+    src: ["/pixi-shooter/snd/wall-hit.wav"],
     preload: true,
     volume: 0.2,
   }),
 
   // Agents
   gorilla: new Howl({
-    src: ['/snd/gorilla.wav'],
+    src: ["/pixi-shooter/snd/gorilla.wav"],
     preload: true,
     volume: 0.5,
   }),
 
   // Skills
   haha: new Howl({
-    src: ['/snd/haha.wav'],
+    src: ["/pixi-shooter/snd/haha.wav"],
     preload: true,
   }),
 
   // Others
   clone: new Howl({
-    src: ['/snd/clone.mp3'],
+    src: ["/pixi-shooter/snd/clone.mp3"],
     preload: true,
   }),
 };
--- src/game/textures.ts
+++ src/game/textures.ts
@@ -1,4 +1,4 @@
-import * as PIXI from 'pixi.js';
+import * as PIXI from "pixi.js";
 
 // Disables the sprite smoothing
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
@@ -9,33 +9,37 @@ interface ITextures {
 
 const textures: ITextures = {
   // Agents
-  char1: PIXI.Texture.from('/img/char1.png'),
-  char2: PIXI.Texture.from('/img/char2.png'),
-  char3: PIXI.Texture.from('/img/char3.png'),
+  char1: PIXI.Texture.from("/pixi-shooter/img/char1.png"),
+  char2: PIXI.Texture.from("/pixi-shooter/img/char2.png"),
+  char3: PIXI.Texture.from("/pixi-shooter/img/char3.png"),
 
   // Projectiles
-  'projectile-bullet': PIXI.Texture.from('/img/projectile-bullet.png'),
-  'projectile-power-ball': PIXI.Texture.from('/img/projectile-power-ball.png'),
+  "projectile-bullet": PIXI.Texture.from(
+    "/pixi-shooter/img/projectile-bullet.png"
+  ),
+  "projectile-power-ball": PIXI.Texture.from(
+    "/pixi-shooter/img/projectile-power-ball.png"
+  ),
 
   // Weapons
-  'weapon-ak47': PIXI.Texture.from('/img/weapon-ak47.png'),
-  'weapon-shotgun': PIXI.Texture.from('/img/weapon-shotgun.png'),
-  'weapon-power': PIXI.Texture.from('/img/weapon-power.png'),
+  "weapon-ak47": PIXI.Texture.from("/pixi-shooter/img/weapon-ak47.png"),
+  "weapon-shotgun": PIXI.Texture.from("/pixi-shooter/img/weapon-shotgun.png"),
+  "weapon-power": PIXI.Texture.from("/pixi-shooter/img/weapon-power.png"),
 
   // Walls
-  pot: PIXI.Texture.from('/img/pot.png'),
+  pot: PIXI.Texture.from("/pixi-shooter/img/pot.png"),
 
   // Items
-  'item-money': PIXI.Texture.from('/img/item-money.png'),
+  "item-money": PIXI.Texture.from("/pixi-shooter/img/item-money.png"),
 
   // Shields
-  'shield-projectile-reflector': PIXI.Texture.from(
-    '/img/shield-projectile-reflector.png'
+  "shield-projectile-reflector": PIXI.Texture.from(
+    "/pixi-shooter/img/shield-projectile-reflector.png"
   ),
 
   // Others
-  key: PIXI.Texture.from('/img/key.png'),
-  tile: PIXI.Texture.from('/img/tile.png'),
+  key: PIXI.Texture.from("/pixi-shooter/img/key.png"),
+  tile: PIXI.Texture.from("/pixi-shooter/img/tile.png"),
 };
 
 export default textures;
