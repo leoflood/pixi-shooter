83b79061d663b59e61f763b75325f03154aee56b
unknown <leoflood98@gmail.com>
2022-09-19 12:12:41 +0100

    Fix window.replace

--- src/views/game/index.tsx
+++ src/views/game/index.tsx
@@ -17,7 +17,7 @@ export default function GameView() {
     startGame(levelKey, appContext.isTouch, () => {
       // When the game ends, it will redirect to levels menu
       // We use window.location.replace here to remove all previous data
-      window.location.replace("/#/levels-menu");
+      window.location.replace("/pixi-shooter/#/levels-menu");
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [levelKey]);
